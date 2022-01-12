bcb67db4ab9c393ad018e083825680e2cae61545
Felipe <felipe_fmachado@hotmail.com>
2022-01-12 17:24:15 -0300

    adicionado o componente ciclo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule,
