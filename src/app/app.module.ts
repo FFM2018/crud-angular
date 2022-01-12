228685ff306d64fb7b235a5176507ab09d721969
Felipe <felipe_fmachado@hotmail.com>
2022-01-12 17:30:26 -0300

    componente ciclo removido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CicloComponent
   ],
   imports: [
     BrowserModule,
